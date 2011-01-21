0b0a30bfe42daefd743161a8a7d51439953cd58e
Troy Thompson <troynt@gmail.com>
2011-01-21 13:40:38 -0500

    removing machine name from prompt.

--- zsh/themes/simple.zsh
+++ zsh/themes/simple.zsh
@@ -2,7 +2,7 @@ function machine_name {
   [ -f ~/.machine_name ] && cat ~/.machine_name || hostname -s
 }
 
-PROMPT='%{$fg[yellow]%}$(machine_name)%{$reset_color%} in %{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)
+PROMPT='%{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)
 $ '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
