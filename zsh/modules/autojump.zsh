45148589231a59f1f8f94b840906b5907804cb12
Brett Buddin <brett@brettbuddin.us>
2012-12-30 21:02:03 -0500

    Proper sourcing so i'm not yelling at autojump

--- zsh/modules/autojump.zsh
+++ zsh/modules/autojump.zsh
@@ -1,3 +1,5 @@
-if [[ $(uname) -eq "Linux" ]]; then
+if [[ $(uname) -eq "Linux" && -s /usr/share/autojump/autojump.sh ]]; then
     source /usr/share/autojump/autojump.sh
+elif [[ $(uname) -eq "Darwin" && -s `brew --prefix`/etc/autojump.sh ]]; then
+    source `brew --prefix`/etc/autojump.sh
 fi
