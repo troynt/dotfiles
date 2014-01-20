cadfcc019ead9f8255c0c0fff051bc2fde0ea1c9
Brett Buddin <brett@buddin.us>
2014-01-19 20:46:53 -0500

    Fixing some git aliases

--- zsh/modules/git.zsh
+++ zsh/modules/git.zsh
@@ -2,7 +2,7 @@ alias gs='git status -sb'
 alias ga='git add'
 alias gd='git diff'
 alias gb='git branch'
-alias gl="git lol"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset%C(yellow)%d%Creset%Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gc='git commit'
-alias gcm='git commit -m'
-alias gco='git checkout'
+alias gca='git commit -a'
+alias gp='git push origin HEAD'
